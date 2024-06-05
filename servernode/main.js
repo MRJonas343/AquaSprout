992a334276bb2ece5f3450c772d6358062020cb4
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-06-05 18:08:31 -0500

    chore: Update npm dependencies

--- servernode/main.js
+++ servernode/main.js
@@ -1,8 +1,13 @@
+let counter = 0
+
 const socket = io()
     socket.on('Humidity', (data) => {
       console.log(`Humidity: ${data}`)
-      document.getElementById('humidity').innerText = data
+      graficoChart.data.labels.push(counter)
+      // biome-ignore lint/complexity/noForEach: <explanation>
+      graficoChart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data.value)
+      })
+      counter++
+      graficoChart.update()
 })
-
-
-
